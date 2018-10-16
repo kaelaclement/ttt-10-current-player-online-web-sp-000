e2f0b02ccf4fd9302a820e6e5c42baf808c31085
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-16 02:56:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,10 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    player = "X"
-  else
+  if turn_count(board).even?
     player = "O"
+  else
+    player = "X"
     return player
   end
 end
